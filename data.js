09c6e7553fd16af22d8a1f065fdfbc4de93e6f5c
sunqdeimos <crimsonsobased@gmail.com>
2026-05-13 16:08:54 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const PRODUCTS = {
+  "item-a": {
+    id: "item-a",
+    name: "Item A",
+    price: 29.99,
+    image: "https://placehold.co/600x600/111/fff?text=Item+A"
+  },
+  "item-b": {
+    id: "item-b",
+    name: "Item B",
+    price: 49.99,
+    image: "https://placehold.co/600x600/111/fff?text=Item+B"
+  },
+  "item-c": {
+    id: "item-c",
+    name: "Item C",
+    price: 19.99,
+    image: "https://placehold.co/600x600/111/fff?text=Item+C"
+  }
+};
