547d4e17d11f31abc0317057ff165442cee3113c
sunqdeimos <crimsonsobased@gmail.com>
2026-05-13 20:51:54 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,20 +1,63 @@
 const PRODUCTS = {
-  "item-a": {
-    id: "item-a",
-    name: "Item A",
-    price: 29.99,
-    image: "https://placehold.co/600x600/111/fff?text=Item+A"
+  s1: {
+    id: "s1",
+    name: "SUNQ A",
+    price: 9.99,
+    image: "https://placehold.co/600x600/111/fff?text=SUNQ+A"
   },
-  "item-b": {
-    id: "item-b",
-    name: "Item B",
-    price: 49.99,
-    image: "https://placehold.co/600x600/111/fff?text=Item+B"
-  },
-  "item-c": {
-    id: "item-c",
-    name: "Item C",
+  s2: {
+    id: "s2",
+    name: "SUNQ B",
     price: 19.99,
-    image: "https://placehold.co/600x600/111/fff?text=Item+C"
+    image: "https://placehold.co/600x600/111/fff?text=SUNQ+B"
+  },
+  s3: {
+    id: "s3",
+    name: "SUNQ C",
+    price: 29.99,
+    image: "https://placehold.co/600x600/111/fff?text=SUNQ+C"
+  },
+  s4: {
+    id: "s4",
+    name: "SUNQ D",
+    price: 39.99,
+    image: "https://placehold.co/600x600/111/fff?text=SUNQ+D"
+  },
+  s5: {
+    id: "s5",
+    name: "SUNQ E",
+    price: 49.99,
+    image: "https://placehold.co/600x600/111/fff?text=SUNQ+E"
+  },
+
+  j1: {
+    id: "j1",
+    name: "JET F",
+    price: 9.99,
+    image: "https://placehold.co/600x600/111/fff?text=JET+F"
+  },
+  j2: {
+    id: "j2",
+    name: "JET G",
+    price: 19.99,
+    image: "https://placehold.co/600x600/111/fff?text=JET+G"
+  },
+  j3: {
+    id: "j3",
+    name: "JET H",
+    price: 29.99,
+    image: "https://placehold.co/600x600/111/fff?text=JET+H"
+  },
+  j4: {
+    id: "j4",
+    name: "JET I",
+    price: 39.99,
+    image: "https://placehold.co/600x600/111/fff?text=JET+I"
+  },
+  j5: {
+    id: "j5",
+    name: "JET J",
+    price: 49.99,
+    image: "https://placehold.co/600x600/111/fff?text=JET+J"
   }
 };
